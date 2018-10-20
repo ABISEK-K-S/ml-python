7777970ba6c49b65c45ebc349e88f8f0fc26c44f
ABISEK-K-S <39756233+ABISEK-K-S@users.noreply.github.com>
2018-10-20 14:10:50 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,74 @@
+
+
+
+
+def match_ends(words):
+    a=0
+    for i in words:
+        x=len(i)
+        if(x>=2)and(i[0]==i[-1]):
+            a=a+1
+    return a
+
+
+
+
+def front_x(words):
+    x=[];
+    y=[];
+    for a in words:
+        if a[0]=='x':
+            x.append(a)
+        else:
+            y.append(a)
+    x=sorted(x)
+    y=sorted(y)
+    z=x+y
+    return z
+
+
+
+def last(l):
+  return l[-1]
+def sort_last(tuples):
+  return sorted(tuples,key=last)
+
+
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+
+
+def main():
+  print('match_ends')
+  test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+  test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+  test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+
+  print()
+  print('front_x')
+  test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+       ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+  test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+       ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+  test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+       ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+
+       
+  print()
+  print('sort_last')
+  test(sort_last([(1, 3), (3, 2), (2, 1)]),
+       [(2, 1), (3, 2), (1, 3)])
+  test(sort_last([(2, 3), (1, 2), (3, 1)]),
+       [(3, 1), (1, 2), (2, 3)])
+  test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+       [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+
+
+if __name__ == '__main__':
+  main()
+
